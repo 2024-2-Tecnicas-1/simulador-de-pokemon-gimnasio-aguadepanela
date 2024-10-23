19fd7a9341d0226c33ed9f73d826a867fd7548e6
Jorge Alejandro López Páez <joral2112@hotmail.com>
2024-10-23 14:53:16 -0500

    clase Tipo Pokemon 1.1
    Se agrega los tipos de pokemon

--- src/main/java/simulador/pokemon/TipoPokemon.java
+++ src/main/java/simulador/pokemon/TipoPokemon.java
@@ -1,6 +1,43 @@
 package simulador.pokemon;
 
-public class TipoPokemon {
-    // TODO: Aquí va tu código
+public class TipoPokemon extends Pokemon {
 
+    private final Tipo tipo; // El tipo es final, no puede cambiar una vez asignado
+
+    // Enumeración para los tipos de Pokémon
+    public enum Tipo {
+        FUEGO,
+        AGUA,
+        PLANTA,
+        ELECTRICO,
+        TIERRA,
+        VOLADOR,
+        PSICICO,
+        HIELO,
+        BICHO,
+        ROCA,
+        FANTASMA,
+        DRAGON,
+        HADA,
+        LUCHADOR,
+        NORMAL,
+        ACERO
+    }
+
+    // Constructor
+    public TipoPokemon(String nombre, int salud, int ataque, String estado, int nivel, Tipo tipo) {
+        super(nombre, salud, ataque, estado, nivel); // Llama al constructor 
+        this.tipo = tipo; 
+    }
+
+    // Método para obtener el tipo de Pokémon
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    // Método toString para mostrar la información del Pokémon junto con su tipo
+    @Override
+    public String toString() {
+        return super.toString() + ", Tipo: " + tipo;
+    }
 }
