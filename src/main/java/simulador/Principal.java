71358545a1b7c718849a3d64cfa6edda5cd91bb0
Jorge Alejandro López Páez <joral2112@hotmail.com>
2024-10-23 14:51:04 -0500

    Modificaciones clases
    Se hace modificaciones a las clase Padre Pokemon y a la clase hija TipoPokemon

--- src/main/java/simulador/Principal.java
+++ src/main/java/simulador/Principal.java
@@ -24,101 +24,101 @@ public class Principal {
 
             switch (opcion) {
                 case 1:
-                System.out.println("Gestionar Entrenador\n-------------------------------");
-                System.out.println("Que desea hacer?");
-                System.out.println("1. Registrar nuevo entrenador\n"
-                        + "2. Ver lista de entrenadores\n"
-                        + "3. Seleccionar un entrenador\n"
-                        + "4. Volver al menu principal");
-                int opcionEntrenador = sc.nextInt();
-        
-                switch (opcionEntrenador) {
-                    case 1:
-                        // Registrar un nuevo entrenador
-                        System.out.print("Ingrese el nombre del nuevo entrenador: ");
-                        String nombreEntrenador = sc.next();
-                        entrenadores.add(nombreEntrenador);
-                        System.out.println("Entrenador " + nombreEntrenador + " registrado.");
-                        break;
-                    case 2:
-                        // Ver la lista de entrenadores
-                        if (entrenadores.isEmpty()) {
-                            System.out.println("No hay entrenadores registrados.");
-                        } else {
-                            System.out.println("Lista de entrenadores:");
-                            for (int i = 0; i < entrenadores.size(); i++) {
-                                System.out.println((i + 1) + ". " + entrenadores.get(i));
-                            }
-                        }
-                        break;
-                    case 3:
-                        // Seleccionar un entrenador
-                        if (entrenadores.isEmpty()) {
-                            System.out.println("No hay entrenadores disponibles.");
-                        } else {
-                            System.out.println("Seleccione un entrenador:");
-                            for (int i = 0; i < entrenadores.size(); i++) {
-                                System.out.println((i + 1) + ". " + entrenadores.get(i));
-                            }
-                            int seleccion = sc.nextInt();
-        
-                            if (seleccion > 0 && seleccion <= entrenadores.size()) {
-                                entrenadorSeleccionado = entrenadores.get(seleccion - 1);
-                                System.out.println("Entrenador seleccionado: " + entrenadorSeleccionado);
-                            } else {
-                                System.out.println("Selección no válida.");
-                            }
-                        }
-                        break;
-                    case 4:
-                        // Volver al menú principal
-                        break;
-                    default:
-                        System.out.println("Opción no válida.");
-                }
-                    break;
-                case 2:
-                if (entrenadorSeleccionado == null) {
-                    System.out.println("No hay entrenador seleccionado.");
-                    return;
-                }
-        
-                System.out.println("Gestionar Pokémones\n-------------------------------");
-                int opcionGestionPokemon;
-        
-                do {
-                    System.out.println("\nGestión de equipo del entrenador " + entrenadorSeleccionado);
-                    System.out.println("1. Ver equipo de Pokémones");
-                    System.out.println("2. Agregar Pokémon al equipo");
-                    System.out.println("3. Entrenar Pokémon");
-                    System.out.println("4. Volver a Gestionar Entrenadores");
-                    System.out.print("Elige una opción: ");
-                    opcionGestionPokemon = sc.nextInt();
-        
-                    switch (opcionGestionPokemon) {
+                    System.out.println("Gestionar Entrenador\n-------------------------------");
+                    System.out.println("Que desea hacer?");
+                    System.out.println("1. Registrar nuevo entrenador\n"
+                            + "2. Ver lista de entrenadores\n"
+                            + "3. Seleccionar un entrenador\n"
+                            + "4. Volver al menu principal");
+                    int opcionEntrenador = sc.nextInt();
+
+                    switch (opcionEntrenador) {
                         case 1:
-                            System.out.println("Mostrando el equipo de Pokémon del entrenador...");
-                            
+                            // Registrar un nuevo entrenador
+                            System.out.print("Ingrese el nombre del nuevo entrenador: ");
+                            String nombreEntrenador = sc.next();
+                            entrenadores.add(nombreEntrenador);
+                            System.out.println("Entrenador " + nombreEntrenador + " registrado.");
                             break;
                         case 2:
-                            System.out.println("Agregando un Pokémon al equipo...");
-                           
+                            // Ver la lista de entrenadores
+                            if (entrenadores.isEmpty()) {
+                                System.out.println("No hay entrenadores registrados.");
+                            } else {
+                                System.out.println("Lista de entrenadores:");
+                                for (int i = 0; i < entrenadores.size(); i++) {
+                                    System.out.println((i + 1) + ". " + entrenadores.get(i));
+                                }
+                            }
                             break;
                         case 3:
-                            System.out.println("Entrenando Pokémon...");
-                          
+                            // Seleccionar un entrenador
+                            if (entrenadores.isEmpty()) {
+                                System.out.println("No hay entrenadores disponibles.");
+                            } else {
+                                System.out.println("Seleccione un entrenador:");
+                                for (int i = 0; i < entrenadores.size(); i++) {
+                                    System.out.println((i + 1) + ". " + entrenadores.get(i));
+                                }
+                                int seleccion = sc.nextInt();
+
+                                if (seleccion > 0 && seleccion <= entrenadores.size()) {
+                                    entrenadorSeleccionado = entrenadores.get(seleccion - 1);
+                                    System.out.println("Entrenador seleccionado: " + entrenadorSeleccionado);
+                                } else {
+                                    System.out.println("Selección no válida.");
+                                }
+                            }
                             break;
                         case 4:
-                            System.out.println("Volviendo a Gestión de Entrenadores...");
+                            // Volver al menú principal
                             break;
                         default:
                             System.out.println("Opción no válida.");
                     }
-                } while (opcionGestionPokemon != 4);
+                    break;
+                case 2:
+                    if (entrenadorSeleccionado == null) {
+                        System.out.println("No hay entrenador seleccionado.");
+                        return;
+                    }
+
+                    System.out.println("Gestionar Pokémones\n-------------------------------");
+                    int opcionGestionPokemon;
+
+                    do {
+                        System.out.println("\nGestión de equipo del entrenador " + entrenadorSeleccionado);
+                        System.out.println("1. Ver equipo de Pokémones");
+                        System.out.println("2. Agregar Pokémon al equipo");
+                        System.out.println("3. Entrenar Pokémon");
+                        System.out.println("4. Volver a Gestionar Entrenadores");
+                        System.out.print("Elige una opción: ");
+                        opcionGestionPokemon = sc.nextInt();
+
+                        switch (opcionGestionPokemon) {
+                            case 1:
+                                System.out.println("Mostrando el equipo de Pokémon del entrenador...");
+
+                                break;
+                            case 2:
+                                System.out.println("Agregando un Pokémon al equipo...");
+
+                                break;
+                            case 3:
+                                System.out.println("Entrenando Pokémon...");
+
+                                break;
+                            case 4:
+                                System.out.println("Volviendo a Gestión de Entrenadores...");
+                                break;
+                            default:
+                                System.out.println("Opción no válida.");
+                        }
+                    } while (opcionGestionPokemon != 4);
                     break;
                 case 3:
                     System.out.println("Iniciar Batalla\n-------------------------------");
-                   
+
                     break;
                 case 4:
                     System.out.println("Saliendo...");
@@ -129,6 +129,4 @@ public class Principal {
         } while (opcion != 4);
     }
 
-   
-       
 }
--- src/main/java/simulador/pokemon/Pokemon.java
+++ src/main/java/simulador/pokemon/Pokemon.java
@@ -1,5 +1,63 @@
 package simulador.pokemon;
 
 public class Pokemon {
-    // TODO: Aquí va tu código
+
+    private String nombre;
+    private int salud;
+    private int ataque;
+    private String estado;
+    private int nivel;
+
+    // Constructor
+    public Pokemon(String nombre, int salud, int ataque, String estado, int nivel) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.ataque = ataque;
+        this.estado = estado;
+        this.nivel = nivel;
+    }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    // Setters
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setSalud(int salud) {
+        this.salud = salud;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    
 }
