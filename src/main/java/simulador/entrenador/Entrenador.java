1d6a0bfcbbe68ec0e23a932e21994644df46a9e1
Jorge Alejandro López Páez <joral2112@hotmail.com>
2024-11-13 21:16:04 -0500

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/simulador/entrenador/Entrenador.java
+++ src/main/java/simulador/entrenador/Entrenador.java
@@ -4,15 +4,32 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Entrenador {
+<<<<<<< Updated upstream
     final private String nombre;
+=======
+
+    public static void listaEntrenadores(LinkedList<Entrenador> entrenadores) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public static void agregarEntrenador(LinkedList<Entrenador> entrenadores) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public static void agregarEntrenador(LinkedList<Entrenador> entrenadores) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    private String nombre;
+>>>>>>> Stashed changes
     private static Scanner sc = new Scanner(System.in);  
+    private static LinkedList<Entrenador> entrenadores = new LinkedList<>(); // Lista estática para almacenar entrenadores
 
     // Constructor
     public Entrenador(String nombre) {
         this.nombre = nombre;
     }
 
-    // Método  obtener el nombre
+    // Método para obtener el nombre
     public String getNombre() {
         return nombre;
     }
@@ -23,8 +40,8 @@ public class Entrenador {
         return nombre;
     }
 
-    // Método para agregar entrenadores
-    public static void agregarEntrenador(LinkedList<Entrenador> entrenadores) {
+    // Método para agregar entrenadores a la lista
+    public static void agregarEntrenador() {
         System.out.println("Ingrese el nombre del entrenador:");
         String nombreEntrenador = sc.nextLine();
         
@@ -34,4 +51,15 @@ public class Entrenador {
         
         System.out.println("Entrenador '" + nombreEntrenador + "' agregado a la lista.");
     }
+<<<<<<< Updated upstream
+=======
+
+    // Método  los entrenadores
+    public static void listaEntrenadores() {
+        System.out.println("Número de entrenadores: " + entrenadores.size());
+        for (Entrenador entrenador : entrenadores) {
+            System.out.println(entrenador);
+        }
+    }
+>>>>>>> Stashed changes
 }
