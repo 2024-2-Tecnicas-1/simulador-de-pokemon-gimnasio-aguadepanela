35132e993fe96bae7b7cbfe3fc3fa5df9b87a22b
jorgealopezp <159073082+jorgealopezp@users.noreply.github.com>
2024-11-13 21:15:39 -0500

    Actualizacion Entrenador

--- src/main/java/simulador/entrenador/Entrenador.java
+++ src/main/java/simulador/entrenador/Entrenador.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Entrenador {
-    private String nombre;
+    final private String nombre;
     private static Scanner sc = new Scanner(System.in);  
 
     // Constructor
